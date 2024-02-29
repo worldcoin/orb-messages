f60a87cccbd8dedc0ffc5e1e24ea36c2bf032764
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2024-02-29 21:04:16 +0100

    rust: add common ack error codes definitions (#6)
    * rust: add common ack error codes definitions
    
    for both mcu, for easier handling of the ack messages
    
    * rust: shortcut to access ErrorCode
    
    makes reading more pleasant
    
    Co-authored-by: Ryan Butler <thebutlah@gmail.com>
    
    * rust: shortcut to access ErrorCode
    
    makes reading more pleasant
    
    Co-authored-by: Ryan Butler <thebutlah@gmail.com>
    
    ---------
    
    Co-authored-by: Ryan Butler <thebutlah@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![allow(clippy::all, clippy::pedantic)]
 
+use thiserror::Error;
+
 pub mod mcu_main;
 pub mod mcu_sec;
 
@@ -30,5 +32,78 @@ impl core::fmt::Display for mcu_sec::ack::ErrorCode {
             Self::OperationNotSupported => "operation is not supported",
             Self::InvalidState => "message can not be processed because of the current state",
         })
+    }
+}
+
+/// Common acknowledgement error codes for _any_ microcontroller.
+#[derive(Error, Debug)]
+pub enum CommonAckError {
+    #[error("operation succeeded")]
+    Success,
+    #[error("unsupported version")]
+    Version,
+    #[error("argument is out of range")]
+    Range,
+    #[error("operation is already in progress")]
+    InProgress,
+    #[error("operation failed")]
+    Fail,
+    #[error("operation could not be completed because of overheating")]
+    OverTemperature,
+    #[error("operation is not supported")]
+    OperationNotSupported,
+    #[error("message can not be processed because of the current state")]
+    InvalidState,
+}
+
+impl From<i32> for CommonAckError {
+    fn from(value: i32) -> Self {
+        match value {
+            0 => CommonAckError::Success,
+            1 => CommonAckError::Version,
+            2 => CommonAckError::Range,
+            3 => CommonAckError::InProgress,
+            4 => CommonAckError::Fail,
+            5 => CommonAckError::OverTemperature,
+            6 => CommonAckError::OperationNotSupported,
+            7 => CommonAckError::InvalidState,
+            _ => {
+                panic!("Unknown error code: {}", value)
+            }
+        }
+    }
+}
+
+/// Converts a `mcu_main::ack::ErrorCode` to a `CommonAckError`.
+impl From<mcu_main::ack::ErrorCode> for CommonAckError {
+    fn from(value: mcu_main::ack::ErrorCode) -> Self {
+        use mcu_main::ack::ErrorCode as E;
+        match value {
+            E::Success => Self::Success,
+            E::Version => Self::Version,
+            E::Range => Self::Range,
+            E::InProgress => Self::InProgress,
+            E::Fail => Self::Fail,
+            E::OverTemperature => Self::OverTemperature,
+            E::OperationNotSupported => Self::OperationNotSupported,
+            E::InvalidState => Self::InvalidState,
+        }
+    }
+}
+
+/// Converts a `mcu_sec::ack::ErrorCode` to a `CommonAckError`.
+impl From<mcu_sec::ack::ErrorCode> for CommonAckError {
+    fn from(value: mcu_sec::ack::ErrorCode) -> Self {
+        use mcu_sec::ack::ErrorCode as E;
+        match value {
+            E::Success => Self::Success,
+            E::Version => Self::Version,
+            E::Range => Self::Range,
+            E::InProgress => Self::InProgress,
+            E::Fail => Self::Fail,
+            E::OverTemperature => Self::OverTemperature,
+            E::OperationNotSupported => Self::OperationNotSupported,
+            E::InvalidState => Self::InvalidState,
+        }
     }
 }
