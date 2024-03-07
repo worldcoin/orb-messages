e0074046efb9e3a8b2bb17f93d4c873f408fda1a
Ryan Butler <ryan.butler@toolsforhumanity.com>
2024-03-07 10:59:46 -0500

    Added more developer tools to flake (#11)

--- flake.nix
+++ flake.nix
@@ -25,6 +25,22 @@
           # Nix makes the following list of dependencies available to the development
           # environment.
           buildInputs = (with p.native; [
+            # Needed for cargo zigbuild
+            zig
+            cargo-zigbuild
+
+            # Used by various rust build scripts to find system libs
+            # Note that this is the unwrapped version of pkg-config. By default,
+            # nix wraps pkg-config with a script that replaces the PKG_CONFIG_PATH
+            # with the proper settings for cross compilation. We already set these
+            # env variables ourselves and don't want nix overwriting them, so we
+            # use the unwrapped version.
+            pkg-config-unwrapped
+
+            # Developer tools
+            cargo-deny
+            cargo-expand
+            cargo-binutils
             protobuf
             nixpkgs-fmt
             pre-commit
