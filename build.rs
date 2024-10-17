8230d5f9d2d09c08014cb52f295c0f1608ea5ac0
Ryan Butler <thebutlah@gmail.com>
2024-10-17 15:40:08 -0400

    fix caching issue

--- build.rs
+++ build.rs
@@ -6,8 +6,8 @@ fn main() {
     // NOTE: We cannot use `CARGO_MANIFEST_DIR`, because protoc doesn't work well with
     // absolute paths.
     #[allow(clippy::eq_op)]
-    let is_public =
-        env!("CARGO_PKG_REPOSITORY") == "https://github.com/worldcoin/orb-messages";
+    let is_public = std::env::var("CARGO_PKG_REPOSITORY").unwrap()
+        == "https://github.com/worldcoin/orb-messages";
     let (messages_dir, priv_dir) = if is_public {
         println!("cargo:warning=Be aware that private definitions are stubbed out when building the public crate.");
 
@@ -18,20 +18,12 @@ fn main() {
 
     // rebuild if any of this changes
     println!("cargo:rerun-if-env-changed=PROTOC");
-    println!("cargo:rerun-if-env-changed=build.rs");
     println!("cargo:rerun-if-env-changed=PROTOC_INCLUDE");
     println!("cargo:rerun-if-changed={}", messages_dir.display());
     println!("cargo:rerun-if-changed={}", priv_dir.display());
 
     // These protos and any others that are imported by them will get compiled
-    if let Err(err) = || -> std::io::Result<()> {
-        prost_build::Config::default().compile_protos(
-            &[messages_dir.join("orb.proto")],
-            &[messages_dir, priv_dir],
-        )?;
-
-        Ok(())
-    }() {
-        panic!("{err}");
-    }
+    prost_build::Config::default()
+        .compile_protos(&[messages_dir.join("orb.proto")], &[messages_dir, priv_dir])
+        .expect("failed to compile protobufs");
 }
