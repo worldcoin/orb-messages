44de6d08fb329da43db9276866d51effc6595fcf
Ryan Butler <thebutlah@gmail.com>
2024-10-21 11:41:09 -0400

    Deduplicate common messages (#37)
    * Deduplicate common messages
    
    * proto: fix compilation with nanopb (#38)
    
    * swtich to orb.mcu package prefix
    
    * fix caching issue
    
    * build: cmake fix order of inclusion
    
    * build: keep empty orb.options file
    
    to make generation from Makefile in private repo works
    
    * change orb.proto to mcu.proto
    
    * build: cmake: change orb.proto to mcu.proto
    
    ---------
    
    Co-authored-by: Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>

--- build.rs
+++ build.rs
@@ -6,8 +6,8 @@ fn main() {
     // NOTE: We cannot use `CARGO_MANIFEST_DIR`, because protoc doesn't work well with
     // absolute paths.
     #[allow(clippy::eq_op)]
-    let is_public =
-        env!("CARGO_PKG_REPOSITORY") == "https://github.com/worldcoin/orb-messages";
+    let is_public = std::env::var("CARGO_PKG_REPOSITORY").unwrap()
+        == "https://github.com/worldcoin/orb-messages";
     let (messages_dir, priv_dir) = if is_public {
         println!("cargo:warning=Be aware that private definitions are stubbed out when building the public crate.");
 
@@ -22,26 +22,8 @@ fn main() {
     println!("cargo:rerun-if-changed={}", messages_dir.display());
     println!("cargo:rerun-if-changed={}", priv_dir.display());
 
-    let main_proto = messages_dir.join("mcu_messaging_main.proto");
-    let sec_proto = messages_dir.join("mcu_messaging_sec.proto");
-    let sec_priv_proto = priv_dir.join("mcu_messaging_sec_priv.proto");
-
-    // Codegen with protoc.
-    if let Err(err) = || -> std::io::Result<()> {
-        prost_build::Config::default()
-            .default_package_filename("mcu_messaging_main")
-            .compile_protos(&[main_proto], &[messages_dir])?;
-
-        prost_build::Config::default()
-            .default_package_filename("mcu_messaging_sec")
-            .compile_protos(&[sec_proto], &[messages_dir, &priv_dir])?;
-
-        prost_build::Config::default()
-            .default_package_filename("mcu_messaging_sec_priv")
-            .compile_protos(&[sec_priv_proto], &[priv_dir])?;
-
-        Ok(())
-    }() {
-        panic!("{err}");
-    }
+    // These protos and any others that are imported by them will get compiled
+    prost_build::Config::default()
+        .compile_protos(&[messages_dir.join("mcu.proto")], &[messages_dir, priv_dir])
+        .expect("failed to compile protobufs");
 }
