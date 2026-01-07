16ccdd482d310fc5c1f2409b06a7a8d7093d40b6
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2026-01-07 17:19:27 +0100

    fix(nix): to fix ci
    issue with licensing

--- flake.nix
+++ flake.nix
@@ -1,66 +1,19 @@
 {
-  description = "orb-messages flake";
-
+  description = "orb-internal flake";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    # Provides eachDefaultSystem and other utility functions
-    utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
+    orb-software.url = "github:worldcoin/orb-software";
   };
 
-  outputs = { self, nixpkgs, utils }:
-
+  outputs = { self, flake-utils, orb-software }:
     # This helper function is used to more easily abstract
     # over the host platform.
     # See https://github.com/numtide/flake-utils#eachdefaultsystem--system---attrs
-    utils.lib.eachDefaultSystem (system:
-      let
-        p = {
-          # The platform that you are running nix on and building from
-          native = nixpkgs.legacyPackages.${system};
-        };
-      in
+    flake-utils.lib.eachDefaultSystem (system:
+      # See https://nixos.wiki/wiki/Flakes#Output_schema
       {
-        # Everything in here becomes your shell (nix develop)
-        devShells.default = p.native.mkShell {
-          # Nix makes the following list of dependencies available to the development
-          # environment.
-          buildInputs = (with p.native; [
-            # Needed for cargo zigbuild
-            zig
-            cargo-zigbuild
-
-            # Used by various rust build scripts to find system libs
-            # Note that this is the unwrapped version of pkg-config. By default,
-            # nix wraps pkg-config with a script that replaces the PKG_CONFIG_PATH
-            # with the proper settings for cross compilation. We already set these
-            # env variables ourselves and don't want nix overwriting them, so we
-            # use the unwrapped version.
-            pkg-config-unwrapped
-
-            # Developer tools
-            clang-tools
-            cargo-binutils
-            cargo-deny
-            cargo-expand
-            nixpkgs-fmt
-            pre-commit
-            protobuf
-
-            # This is missing on mac m1 nix, for some reason.
-            # see https://stackoverflow.com/a/69732679
-            libiconv
-          ]);
-
-          # The following sets up environment variables for the shell. These are used
-          # by the build.rs build scripts of the rust crates.
-          shellHook = ''
-            # Env vars here
-          '';
-        };
-        # Lets you type `nix fmt` to format the flake.
-        formatter = p.native.nixpkgs-fmt;
+        devShells = orb-software.devShells.${system};
+        formatter = orb-software.formatter.${system};
       }
-
     );
-
 }
